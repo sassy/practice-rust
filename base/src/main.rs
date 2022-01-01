00c2babfd2e7f5f60746928f7c007a1b513badf1
Satoshi Watanabe <sassy.watanabe@gmail.com>
2022-01-01 22:58:30 +0900

    制御文の例を追加

---
+++ base/src/main.rs
@@ -0,0 +1,48 @@
+
+fn kurikaeshi(v: Vec<i32>) {
+    for i in &v {
+        print!("{} ", i);
+    }
+    println!("");
+}
+
+fn kurikaeshi_iter(v: Vec<i32>) {
+    for i in v.iter() {
+        print!("{} ", i);
+    }
+    println!("");
+}
+
+fn kurikaeshi_iter_with_idx(v: Vec<i32>) {
+    for (i, x) in v.iter().enumerate() {
+        print!("{}:{} ", i, x);
+    }
+    println!("");
+}
+
+fn test(x: i32, y: i32) -> bool {
+    x > y
+}
+
+// if文の例
+fn condition(a :i32, b: i32) {
+    if a == b {
+        println!("a == b is ok");
+    } else if test(a, b) {
+        println!("a < b is ok");
+    } else {
+        println!("a < b is ng");
+    }
+}
+
+fn main() {
+    condition(10, 20);
+    condition(20, 10);
+    condition(10, 10);
+    let v1 = vec![10,20, 30];
+    kurikaeshi(v1);
+    let v2 = vec![10,20, 30];
+    kurikaeshi_iter(v2);
+    let v3 = vec![10,20, 30];
+    kurikaeshi_iter_with_idx(v3);
+}
